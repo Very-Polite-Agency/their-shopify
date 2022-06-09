ec0c6db7581388d7ec5a46484ee699cc2bb88535
Brenden Mersey <brenden.mersey@weareverypolite.com>
2022-06-09 10:04:41 -0700

    Final pass on the mobile menu, including document event click watcher to close, not just burger.

--- js/modules/_mobileMenu.js
+++ js/modules/_mobileMenu.js
@@ -9,17 +9,35 @@ const MobileMenu = (() => {
 
   let tools = new Tools();
   let elements = tools.getArrayOfElementsByTag();
+  let trigger = document.querySelectorAll('.js--mobile-menu-trigger') || [];
 
   //////////////////////////////////////////////////////////
   ////  Toggle Mobile Menu
   //////////////////////////////////////////////////////////
 
   const toggleMobileMenu = () => {
-    ( document.querySelectorAll('.js--mobile-menu-trigger') || [] ).forEach( trigger => {
-      trigger.addEventListener('click',() => {
+    trigger.forEach( el => {
+      el.addEventListener('click',() => {
         tools.toggleClass( 'mobile-menu--active', elements );
-        trigger.classList.toggle( 'is-active' );
+        el.classList.toggle( 'is-active' );
       });
+    });
+  };
+
+  //////////////////////////////////////////////////////////
+  ////  Close Menu
+  //////////////////////////////////////////////////////////
+
+  const closeMenu = () => {
+    document.body.addEventListener('click', event => {
+      let isMobileMenu = event.target.closest('#shopify-section-mobile-menu') ? true : false;
+      let isMobileMenuTrigger = event.target.closest('.js--mobile-menu-trigger') ? true : false;
+      if ( !isMobileMenu && !isMobileMenuTrigger ) {
+        tools.removeClass( 'mobile-menu--active', elements );
+        trigger.forEach( el => {
+          el.classList.remove( 'is-active' );
+        })
+      }
     });
   };
 
@@ -29,6 +47,7 @@ const MobileMenu = (() => {
 
   const init = ( $options = false ) => {
     if ( debug ) console.log( `${info.name}.init() Started` );
+    closeMenu();
     toggleMobileMenu();
     if ( debug ) console.log( `${info.name}.init() Finished` );
   };
